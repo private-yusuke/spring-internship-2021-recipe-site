89e067d356015ca401bb93a9818d4527d343e01d
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2021-03-25 21:30:29 +0900

    Remove unnecessary constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,11 +6,6 @@ export const ORIGIN_API_ENDPOINT_SEARCH = `${ORIGIN_API_SERVER_ROOT}/search`;
 export const ORIGIN_API_ENDPOINT_IMAGE_URLS = `${ORIGIN_API_SERVER_ROOT}/image_urls`;
 export const ORIGIN_API_HEADER_X_API_KEY = "X-Api-Key";
 
-export const CLIENT_API_SERVER_ROOT = getCurrentFullUrl("/api");
-export const CLIENT_API_ENDPOINT_RECIPES = `${CLIENT_API_SERVER_ROOT}/recipes`;
-export const CLIENT_API_ENDPOINT_SEARCH = `${CLIENT_API_SERVER_ROOT}/search`;
-export const CLIENT_API_ENDPOINT_IMAGE_URLS = `${CLIENT_API_SERVER_ROOT}/image_urls`;
-
 export const BOOKMARK_DB_NAME = "bookmarkDB";
 export const BOOKMARK_DB_VERSION = 1;
 export const BOOKMARK_DB_RECIPE_LIST_NAME = "recipeIDs";
