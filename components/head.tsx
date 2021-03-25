2e17960ab75b7e0c93ce0eb7d5d6fc08443afd96
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2021-03-25 22:32:44 +0900

    Add favicon

--- components/head.tsx
+++ components/head.tsx
@@ -32,6 +32,29 @@ const Head: FC<Props> = (props) => {
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={image} />
       <link rel="canonical" href={url} />
+
+      {/* https://realfavicongenerator.net/ */}
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#00aba9" />
+      <meta name="theme-color" content="#ffffff" />
     </NHead>
   );
 };
