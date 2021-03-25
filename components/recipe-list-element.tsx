bf086ad7fc42ac1a45555290ba3a68a050ad64c1
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2021-03-25 19:40:10 +0900

    Add bookmark button for each element of recipe list

--- components/recipe-list-element.tsx
+++ components/recipe-list-element.tsx
@@ -1,18 +1,58 @@
 import Link from "next/link";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { Recipe } from "../lib/recipe";
 import Image from "next/image";
+import {
+  initializeBookmark,
+  isInBookmark,
+  toggleBookmark,
+} from "../lib/client/bookmark";
+
+type BookmarkState =
+  | "Loading"
+  | "NewNotBookmarked"
+  | "NotBookmarked"
+  | "Bookmarked"
+  | "NewBookmarked"
+  | "Error";
 
 /**
  * レシピ一覧での各レシピの表示に利用するコンポーネント
  */
 const RecipeListElement: FC<{ recipe: Recipe }> = ({ children, recipe }) => {
+  const [bookmarkState, setBookmarkState] = useState<BookmarkState>("Loading");
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await initializeBookmark();
+        const isBookmarked = await isInBookmark(recipe.id);
+        setBookmarkState(isBookmarked ? "Bookmarked" : "NotBookmarked");
+      } catch (e) {
+        setBookmarkState("Error");
+        return;
+      }
+    })();
+  }, []);
+
+  const onBookmarkButtonClicked = async (_) => {
+    let bookmarked: boolean;
+    try {
+      bookmarked = await toggleBookmark(recipe);
+    } catch (e) {
+      console.error(e);
+      setBookmarkState("Error");
+      return;
+    }
+    setBookmarkState(bookmarked ? "NewBookmarked" : "NewNotBookmarked");
+  };
+
   return (
-    <Link href={`recipes/${recipe.id}`}>
-      <div
-        className="recipe-element-container flex items-center m-4"
-        key={recipe.id}
-      >
+    <div
+      className="recipe-element-container flex items-center m-4"
+      key={recipe.id}
+    >
+      <Link href={`recipes/${recipe.id}`}>
         <div className="recipe-image-container mr-4 mt-4 flex-1 text-center">
           {recipe.image_url ? (
             <Image
@@ -27,12 +67,37 @@ const RecipeListElement: FC<{ recipe: Recipe }> = ({ children, recipe }) => {
             <p className="text-8xl text-center">🍽️</p>
           )}
         </div>
-        <div className="recipe-summary flex-1 mt-4">
-          <h2 className="recipe-title text-l mb-2">{recipe.title}</h2>
-          <p className="recipe-description text-sm">{recipe.description}</p>
+      </Link>
+      <div className="recipe-summary flex-1 mt-4">
+        <Link href={`recipes/${recipe.id}`}>
+          <div>
+            <h2 className="recipe-title text-l mb-2">{recipe.title}</h2>
+            <p className="recipe-description text-sm">{recipe.description}</p>
+          </div>
+        </Link>
+        <div className="flex mt-2">
+          <button onClick={onBookmarkButtonClicked}>
+            {bookmarkState === "Bookmarked" || bookmarkState === "NewBookmarked"
+              ? "✔"
+              : bookmarkState === "Loading"
+              ? "⌛"
+              : bookmarkState === "NotBookmarked" ||
+                bookmarkState == "NewNotBookmarked"
+              ? "📌"
+              : bookmarkState === "Error"
+              ? "❌ "
+              : "Unexpected behaviour"}
+          </button>
+          <p className="ml-2 mt-1 text-sm">
+            {bookmarkState === "NewBookmarked"
+              ? "ブックマーク追加済"
+              : bookmarkState === "NewNotBookmarked"
+              ? "ブックマーク削除済"
+              : null}
+          </p>
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
